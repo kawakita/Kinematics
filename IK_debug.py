e5d864aaa0d926210565fc19fc3970e9e31f68c7
kawakita <kawakita@users.noreply.github.com>
2018-03-07 22:27:25 -0800

    code

--- IK_debug.py
+++ IK_debug.py
@@ -5,8 +5,8 @@ import tf
 
 '''
 Format of test case is [ [[EE position],[EE orientation as quaternions]],[WC location],[joint angles]]
-You can generate additional test cases by setting up your kuka project and running `$ roslaunch kuka_arm forward_kinematics.launch`
-From here you can adjust the joint angles to find thetas, use the gripper to extract positions and orientation (in quaternion xyzw) and lastly use link 5
+You can generate additional test cases by setting up your kuka project and running `$ roslaunch kuka_arm forward_kinematics$
+From here you can adjust the joint angles to find thetas, use the gripper to extract positions and orientation (in quaterni$
 to find the position of the wrist center. These newly generated test cases can be added to the test_cases dictionary.
 '''
 
@@ -58,34 +58,157 @@ def test_code(test_case):
 
     req = Pose(comb)
     start_time = time()
-    
+
     ########################################################################################
-    ## 
+    ##
 
     ## Insert IK code here!
-    
-    theta1 = 0
-    theta2 = 0
-    theta3 = 0
-    theta4 = 0
-    theta5 = 0
-    theta6 = 0
 
-    ## 
+    # link offsets
+    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+    # link lengths
+    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+    # twist angles
+    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+    #j joint angles
+    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+
+    # define DH parameter table
+    DH_Table = { alpha0:      0, a0:      0, d1:  0.75, q1: q1,
+                 alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2. + q2,
+                 alpha2:      0, a2:   1.25, d3:     0, q3: q3,
+                 alpha3: -pi/2., a3: -0.054, d4:   1.5, q4: q4,
+                 alpha4: pi/2., a4:      0, d5:     0, q5: q5,
+                 alpha5: -pi/2., a5:      0, d6:     0, q6: q6,
+                 alpha6:      0, a6:      0, d7: 0.303, q7: 0 }
+
+    # define function for transformation from link i-1 to i
+    def TF_Matrix(alpha, a, d, q):
+      TF = Matrix([[            cos(q),           -sin(q),           0,             a],
+                   [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                   [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                   [                 0,                 0,          0,             1]])
+      return TF
+
+    # individual transformation matrix from link i-1 to i
+    T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
+    T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
+    T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
+    T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(DH_Table)
+    T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(DH_Table)
+    T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
+    T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
+
+    # total transformation matrix from link 0 to the end effector
+    T0_EE = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_EE
+
+
+    # pull in given end effector positions
+    px = req.poses[x].position.x
+    py = req.poses[x].position.y
+    pz = req.poses[x].position.z
+
+
+    # define symbols for roll, pitch, yaw
+    r, p, y = symbols('r p y')
+
+    ROT_x = Matrix([[1,      0,       0 ],
+                    [0, cos(r), -sin(r) ],
+                    [0, sin(r), cos(r)  ]])
+
+    ROT_y = Matrix([[ cos(p), 0, sin(p)],
+                    [      0, 1,     0 ],
+                    [-sin(p), 0, cos(p)]])
+
+    ROT_z = Matrix([[ cos(y), -sin(y), 0],
+                    [ sin(y),  cos(y), 0],
+                    [      0,       0, 1]])
+
+    # create composition of rotations
+    ROT_EE  = ROT_z * ROT_y * ROT_x
+
+    # account for difference in orientation of gripper link frame
+    # first rotate about z-axis by 180 then y-axis by -90
+    ROT_ERROR = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+
+    # get compensated rotation matrix
+    ROT_EE = ROT_EE * ROT_ERROR
+
+    # get roll, pitch, and yaw using transformations module
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+      [req.poses[x].orientation.x, req.poses[x].orientation.y,
+       req.poses[x].orientation.z, req.poses[x].orientation.w])
+
+    ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
+
+    # formulate matrix that defines position of end effector
+    EE = Matrix([[px],[py],[pz]])
+
+    # position of wrist is just end effector translated back by d7
+    WC = EE - 0.303 * ROT_EE[:,2]
+
+    # Inverse Kinematics
+
+    ## INVERSE POSITION
+    # theta1 is easy using wrist center with top down view of arm
+    theta1 = atan2(WC[1],WC[0])
+
+    # look at triangle with sides a, b, c and opposing angles
+    side_a = 1.501
+    side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
+    side_c = 1.25
+
+    # cosine laws, find angles of triangle based on knowing 3 sides
+    angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+    angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+    angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+
+    # based on diagram can now calculate theta2, theta3
+    theta2 = pi / 2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+    theta3 = pi / 2 - (angle_b + 0.036) # 0.036 accounts for drop in link 4 of -0.054m
+
+    ## INVERSE ORIENTATION
+
+    # define rotation matrix
+    R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+    #R3_6 = T3_4[0:3,0:3] * T4_5[0:3,0:3] * T5_6[0:3,0:3]
+    #pprint(R3_6)
+
+    R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
+
+    R3_6 = R0_3.transpose() * ROT_EE
+
+    r12, r13 = R3_6[0,1], R3_6[0,2]
+    r21, r22, r23 = R3_6[1,0], R3_6[1,1], R3_6[1,2]
+    r32, r33 = R3_6[2,1], R3_6[2,2]
+
+    theta5 = atan2(sqrt(r13 * r13 + r33*r33),r23)
+
+    if sin(theta5) < 0:
+       theta4  = atan2(-r33, r13)
+       theta6 = atan2(r22, -r21)
+    else:
+       theta4 = atan2(r33, -r13)
+       theta6 = atan2(-r22, r21)
+
+    ##
     ########################################################################################
-    
+
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
     ## as the input and output the position of your end effector as your_ee = [x,y,z]
 
     ## (OPTIONAL) YOUR CODE HERE!
 
+    FK = T0_EE.evalf(subs={q1: theta1, q2: theta2, q3: theta3, q4: theta4, q5: theta5, q6: theta6})
+
     ## End your code input for forward kinematics here!
     ########################################################################################
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
-    your_wc = [1,1,1] # <--- Load your calculated WC values in this array
-    your_ee = [1,1,1] # <--- Load your calculated end effector value from your forward kinematics
+    your_wc = [WC[0],WC[1],WC[2]] # <--- Load your calculated WC values in this array
+    your_ee = [FK[0,3],FK[1,3],FK[2,3]] # <--- Load your calculated end effector value from your forward kinematics
+    print('your_ee', your_ee)
     ########################################################################################
 
     ## Error analysis
